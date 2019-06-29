c48a69db4e6be454d298ad2aab3f0d8ce02060dd
Narayan Sau <narayansau@yahoo.com>
2019-06-29 18:05:23 +0530

    Initial commit , Spring Boot in 10 Steps

---
+++ src/main/java/com/example/springbootin10step/Springbootin10stepApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootin10step;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootin10stepApplication{
+
+    public static void main(String[] args) {
+        SpringApplication.run( Springbootin10stepApplication.class, args );
+    }
+
+}
